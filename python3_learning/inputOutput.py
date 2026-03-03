884f49d7fe29fd5b7d86fb4393c00fb01435679e
WyattWang <1293110705@qq.com>
2026-03-03 10:33:19 +0800

    committed a folder of python3_learning

---
+++ python3_learning/inputOutput.py
@@ -0,0 +1,12 @@
+# name = input('please enter your name: ')
+# print('hello,' ,name)
+
+#print('1024 * 768 = ',1024 * 768)
+
+# #print absolute value of an integer:
+# x = int(input("Enter a number"))
+# if x < 0:
+#     print(-x)
+# else:
+#     print(x)
+
